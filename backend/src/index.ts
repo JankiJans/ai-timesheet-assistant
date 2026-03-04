32ecd698de0ff285e6f0ea637ecd25deb818dfdf
Jan Kedzierski <jkedzierski087@gmail.com>
2026-03-04 18:46:49 +0100

    add working google gemini client for backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,23 +1,53 @@
 import express, { type Request, type Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { GoogleGenerativeAI } from '@google/generative-ai';
 
-// Wczytujemy zmienne z pliku .env
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
-app.use(express.json()); // Pozwala serwerowi rozumieć format JSON
+app.use(express.json());
 
-// Nasz pierwszy "endpoint" (punkt styku)
+// Inicjalizacja klienta Gemini
+// stała po klucz (TypeScript tego wymaga)
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || '');
+
 app.get('/', (req: Request, res: Response) => {
   res.send('AI Timesheet Assistant API is running! 🚀');
 });
 
-// Start serwera
+//endpoint do czatu
+app.post('/api/chat', async (req: Request, res: Response) => {
+  try {
+    const userMessage = req.body.message;
+
+    if (!userMessage) {
+      res.status(400).json({ error: "Wiadomość jest wymagana!" });
+      return; 
+    }
+
+    //model "Flash" - idealny do szybkich zadań
+    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+
+    //wysyłka wiadomość do AI
+    const result = await model.generateContent(userMessage);
+    const response = await result.response;
+    
+    //Pobieram tekst odpowiedzi
+    const text = response.text();
+
+    //odsyłka wiadomości do testów na Thunder Client
+    res.json({ reply: text || "Brak odpowiedzi od AI" });
+
+  } catch (error) {
+    console.error("Błąd AI:", error);
+    res.status(500).json({ error: "Błąd podczas komunikacji z AI" });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
