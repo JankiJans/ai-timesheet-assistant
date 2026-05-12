c5eeeba237fa4423b0a661d88c8f62fe4e6cc6c0
Jan Kedzierski <jkedzierski087@gmail.com>
2026-05-12 18:35:46 +0200

    fix cors problem on docker

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -18,9 +18,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:5173';
+
   // --- 1. KONFIGURACJA CORS ---
   app.enableCors({
-    origin: 'http://localhost:5173', 
+    origin: frontendUrl, 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
@@ -36,6 +38,7 @@ async function bootstrap() {
   await app.listen(port);
   
   console.log(`🚀 Serwer backendowy NestJS działa na porcie: ${port}`);
+  console.log(`🌍 Akceptuję żądania CORS z adresu: ${frontendUrl}`);
 }
 
 bootstrap();
